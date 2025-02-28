5f3c0dd0f3c2bb66c8c4921de9af8ea0f26619e9
rdfield <raymond.field@mvine.com>
2025-02-28 16:30:43 +0000

    Update CNN_pp.pm
    Add debug option

--- ML/CNN_pp.pm
+++ ML/CNN_pp.pm
@@ -4,6 +4,9 @@ use Modern::Perl;
 use Math::Random qw(random_uniform);
 use Data::Dumper;
 use ML::Util qw(print_2d_array matmul transpose print_1d_array rotate_matrix_180 conv2d add_2_arrays);
+use Cwd qw(abs_path);
+use JSON;
+use File::Slurp;
 
 my $kernel_limit = 15;
 my $c1_kernels_limit = 15;
@@ -31,7 +34,13 @@ sub new {
    }
    # so now we know the size and number of kernels in the first Convolution layer 
    my $kernels;
+   my $c2_kernels = $c1_kernels * 2;
    my $c1_dist = sqrt($c1_kernels/((1 + $c1_kernels) * $kernel_size**2));
+   my $initial_weights;
+   if (defined($args{load_initial_weights}) and $args{load_initial_weights} == 1) {
+      my $json_data = read_file("initial_weights.json");
+      $initial_weights = from_json($json_data);
+   }
    foreach my $p (0 .. $c1_kernels - 1) {
       foreach my $x (0 .. $kernel_size - 1) {
          foreach my $y (0 .. $kernel_size - 1) {
@@ -40,7 +49,30 @@ sub new {
       }
    }
 
-   my $c2_kernels = $c1_kernels * 2;
+# calculate sizes for various layers
+   $self->{c1_kernels} = $c1_kernels;
+   $self->{c2_kernels} = $c2_kernels; # are these known as "filters" in pytorch?
+   $self->{kernel_size} = $kernel_size;
+   $self->{range_upper} = ($self->{kernel_size} % 2 == 0)?($self->{kernel_size} / 2):( ($self->{kernel_size} - 1)/2 );
+   $self->{range_lower} = -1 * $self->{range_upper};
+   my $fc_input_size_side = $input_dimensions->[0]; 
+   if (!$self->{padding}) {
+      $fc_input_size_side -= $self->{range_upper} * 2; # after c1, the data will be kernel_size - 1 smaller each side
+   }
+   $self->{c1_size} = $fc_input_size_side; # needed for backprop
+   $fc_input_size_side /= 2; # after first pooling size will be halved
+   # this is the s1 size, we will need it later in backprop 
+   $self->{s1_size} = $fc_input_size_side;
+   if (!$self->{padding}) {
+      $fc_input_size_side -= $self->{range_upper} * 2; # after c2, the data will be kernel_size - 1 smaller each side
+   }
+   # we will need c2_size on the backprop run
+   $self->{c2_size} = $fc_input_size_side;
+   $fc_input_size_side /= 2; # after second pooling size will be halved
+   $self->{s2_size} = $fc_input_size_side;
+   $self->{fc_input_size} = $fc_input_size_side * $fc_input_size_side * $self->{c2_kernels}; # output of second pooling is a square, and we have c2_kernels of them
+   $self->{fc_output_size} = 10;
+
    my $biases;
    foreach my $p (0 .. $c1_kernels - 1) {
       $biases->[0][$p] = 0;
@@ -59,54 +91,35 @@ sub new {
       }
    }
 
-   $self->{c1_kernels} = $c1_kernels;
-   $self->{c2_kernels} = $c2_kernels; # are these known as "filters" in pytorch?
    $self->{biases} = $biases;
    $self->{input_dimensions} = $input_dimensions;
    $self->{kernels} = $kernels;
-   $self->{kernel_size} = $kernel_size;
+   if (defined($args{load_initial_weights}) and $args{load_initial_weights} == 1) {
+      $self->{kernels} = $initial_weights->{kernels};
+   }
    $self->{c1} = []; # (input_dimension[0] - 2 * range_upper)^2, if no padding
    $self->{c2} = []; # (c1 - 2 * range_upper)^2, if no padding
-   $self->{range_upper} = ($self->{kernel_size} % 2 == 0)?($self->{kernel_size} / 2):( ($self->{kernel_size} - 1)/2 );
-   $self->{range_lower} = -1 * $self->{range_upper};
    $self->{padding} = 0; # for future use, at the moment, no padding is applied, so C1 is kernel_size smaller than the input
 
 # FC weights.  For demo purposes the output layer has 10 values.
-   $self->{fc_output_size} = 10;
    foreach my $b (0 .. $self->{fc_output_size} - 1) {
       $biases->[2][$b] = 0;
    }    
 
-   my $fc_input_size_side = $input_dimensions->[0]; 
-   if (!$self->{padding}) {
-      $fc_input_size_side -= $self->{range_upper} * 2; # after c1, the data will be kernel_size - 1 smaller each side
-   }
-   $self->{c1_size} = $fc_input_size_side; # needed for backprop
-   $fc_input_size_side /= 2; # after first pooling size will be halved
-   # this is the s1 size, we will need it later in backprop 
-   $self->{s1_size} = $fc_input_size_side;
-   if (!$self->{padding}) {
-      $fc_input_size_side -= $self->{range_upper} * 2; # after c2, the data will be kernel_size - 1 smaller each side
-   }
-   # we will need c2_size on the backprop run
-   $self->{c2_size} = $fc_input_size_side;
-   $fc_input_size_side /= 2; # after second pooling size will be halved
-   
-   $self->{fc_input_size} = $fc_input_size_side * $fc_input_size_side * $self->{c2_kernels}; # output of second pooling is a square, and we have c2_kernels of them
-
    my $w_dist = sqrt($c1_kernels/($self->{fc_input_size} + $self->{fc_output_size}));
    $self->{W} = [];
-   foreach my $row ( 0 .. $self->{fc_output_size} - 1) {
-      foreach my $col ( 0 .. $self->{fc_input_size} - 1) {
-          $self->{W}[$row][$col] = random_uniform(1, -1 * $w_dist, $w_dist);
+   if (defined($args{load_initial_weights}) and $args{load_initial_weights} == 1) {
+      $self->{W} = $initial_weights->{weights};
+   } else {
+      foreach my $row ( 0 .. $self->{fc_output_size} - 1) {
+         foreach my $col ( 0 .. $self->{fc_input_size} - 1) {
+             $self->{W}[$row][$col] = random_uniform(1, -1 * $w_dist, $w_dist);
+         }
       }
    }
-   print_2d_array("W", $self->{W}) if $self->{debug};
+   #print_2d_array("W", $self->{W}) if $self->{debug};
    return bless $self, $class;
 
-#   say Dumper($biases->[0]);
-#   say "c1_dist = $c1_dist";
-#   say Dumper($kernels->[0]);
 }
 
 sub load_label {
@@ -117,30 +130,23 @@ sub load_label {
 sub forward {
    my $self = shift;
    my $input = shift;
+   $self->{batch_size} = 1;
 # convolution 1: input to c1 for p kernels
    $self->{input} = $input; # needed for backprop
-   print_2d_array("input", $self->{input}) if $self->{debug};
-   $self->{c1} = [];
-   foreach my $p (0 .. $self->{c1_kernels} - 1) {
-      foreach my $i (0 .. $self->{input_dimensions}[0] - 1 - $self->{range_upper}) { # when no padding is applied, we need to end each row half a kernel width before the end
-         next if ($i < $self->{range_upper}); # when no padding is applied, we need to start each row half a kernel width in
-         foreach my $j (0 .. $self->{input_dimensions}[1] - 1 - $self->{range_upper}) { # when no padding is applied, we need to end each row half a kernel width before the end
-            next if ($j < $self->{range_upper}); # when no padding is applied, we need to start each column half a kernel width in
-            $self->{c1}[$p][$i - $self->{range_upper}][$j - $self->{range_upper}] = 0;
-         }
-      }
-   }
-
+   #print_2d_array("input", $self->{input}) if $self->{debug};
 
    foreach my $p (0 .. $self->{c1_kernels} - 1) {
       $self->{c1}[$p] = conv2d($input, $self->{kernels}[0][$p], "reduce");
+      print_2d_array("c1 kernel $p", $self->{kernels}[0][$p]) if $self->{debug};
+      print_2d_array("c1 before activation $p", $self->{c1}[$p]) if $self->{debug};
       foreach my $i (0 .. $self->{c1_size} - 1) {
          foreach my $j (0 .. $self->{c1_size} - 1) {
             $self->{c1}[$p][$i][$j] = 1 / (1 + exp(-1 * ($self->{c1}[$p][$i][$j] + $self->{biases}[0][$p])));
          }
       }
-      print_2d_array("c1 $p", $self->{c1}[$p]) if $self->{debug};
+      print_2d_array("c1 after activation $p", $self->{c1}[$p]) if $self->{debug};
    }
+
 # pooling layer, p1: reduce c1 by 75%
    my $p1_upper_range = int(($self->{input_dimensions}[0] - 2 * $self->{range_upper}) / 2);
    say "pooling p1 size = $p1_upper_range" if $self->{debug};
@@ -171,6 +177,7 @@ sub forward {
      foreach my $p (0 .. $self->{c1_kernels} - 1) {
         $self->{c2}[$q] = add_2_arrays($self->{c2}[$q], conv2d($self->{p1}[$p], $self->{kernels}[1][$q][$p], "reduce"));
      }
+     print_2d_array("c2 $q before sigmoid", $self->{c2}[$q]) if $self->{debug};
      foreach my $i (0 .. $self->{c2_size} - 1) {
         foreach my $j (0 .. $self->{c2_size} - 1) {
            $self->{c2}[$q][$i][$j] = 1 / ( 1 + exp( -1 * ( $self->{c2}[$q][$i][$j] + $self->{biases}[1][$q] )));
@@ -179,7 +186,6 @@ sub forward {
      print_2d_array("c2 $q after sigmoid", $self->{c2}[$q]) if $self->{debug};
   } 
 
-      
 # 2nd pooling layer, p2: reduce c2 by 75%
    my $p2_upper_range = int(($p1_upper_range - 2 * $self->{range_upper}) / 2);
    $self->{p2_upper_range} = $p2_upper_range;
@@ -225,19 +231,26 @@ sub forward {
    print_2d_array("f", $self->{f}) if $self->{debug};
    $self->{y} = matmul($self->{W}, $self->{f});  
    print_2d_array("y before sigmoid", $self->{y}) if $self->{debug};
+
    print_1d_array("fc biases", $self->{biases}[2]) if $self->{debug};
-   foreach my $row (@{$self->{y}}) {
-      my $bias_idx = 0;
-      foreach my $value ( @{$row} ) {
-         $value = 1/(1 + exp( -1 * ($value + $self->{biases}[2][$bias_idx++])));
+say "fc biases = " . Dumper($self->{biases}[2]);
+foreach my $i (0 .. 9) {
+   say "fc bias $i = " . $self->{biases}[2][$i];
+}
+   foreach my $row (0 .. $self->{fc_output_size} - 1) {
+      foreach my $col (0 .. $self->{batch_size} - 1) {
+say "idx = $row, value = " . $self->{y}->[$row][$col] . ", bias = " . $self->{biases}[2][$row];
+         #$value = 1/(1 + exp( -1 * ($value + $self->{biases}[2][$bias_idx++])));
+         $self->{y}->[$row][$col] = 1/(1 + exp( -1 * ($self->{y}->[$row][$col] + $self->{biases}[2][$row])));
+say "updated value = " . $self->{y}->[$row][$col];
       }
    }
    print_2d_array("y after sigmoid", $self->{y}) if $self->{debug};
-
 }
        
 sub loss {
    my $self = shift;
+
    $self->{loss} = 0;
 print_2d_array("y", $self->{y}) if $self->{debug};
 print_2d_array("label", $self->{label}) if $self->{debug};
@@ -245,6 +258,7 @@ print_2d_array("label", $self->{label}) if $self->{debug};
 say "row = $row, calc = " . $self->{y}[$row][0] . " , label = " . $self->{label}[$row][0] . ", difference = " . ($self->{y}[$row][0] - $self->{label}[$row][0]) if $self->{debug};
        $self->{loss} += 0.5 * ($self->{y}[$row][0] - $self->{label}[$row][0])**2;
    }
+   return $self->{loss};
 }
 
 sub backward {
@@ -255,14 +269,18 @@ sub backward {
    foreach my $row(0 .. $#{$self->{y}}) {
       $self->{delta_bias_fc}[$row][0] = ($self->{y}[$row][0] - $self->{label}[$row][0]) * $self->{y}[$row][0] * (1 - $self->{y}[$row][0]);
    }
+
    print_2d_array("delta fc bias", $self->{delta_bias_fc}) if $self->{debug};
    $self->{delta_W} = matmul($self->{delta_bias_fc}, $f_T);
+
    print_2d_array("W", $self->{W}) if $self->{debug};
    print_2d_array("delta W", $self->{delta_W}) if $self->{debug};
+
    my $W_T = transpose($self->{W});
    $self->{delta_f} = matmul($W_T, $self->{delta_bias_fc});
    print_2d_array("W_T", $W_T) if $self->{debug};
    print_2d_array("delta_f", $self->{delta_f}) if $self->{debug};
+
 # de-vectorisation, turn delta_f back into a series of pooled kernel deltas, i.e. the opposite of "vectorisation" above.
    $self->{delta_p2} ||= [];
    my $row = 0;
@@ -299,23 +317,26 @@ sub backward {
    foreach my $p (0 .. $self->{c1_kernels}) {
       $p1_rot->[$p] = rotate_matrix_180($self->{p1}[$p]);
    }
-
    foreach my $q (0 .. $self->{c2_kernels} - 1) {
       foreach my $p (0 .. $self->{c1_kernels} - 1) {
+         print_2d_array("p1_rot $p", $p1_rot->[$p]) if $self->{debug};
+         print_2d_array("delta_c2_sigmoid $q", $self->{delta_c2_sigmoid}[$q]) if $self->{debug};
          $self->{delta_k2}[$q][$p] = conv2d($p1_rot->[$p], $self->{delta_c2_sigmoid}[$q], "reduce");
          print_2d_array("delta_k2 $q/$p", $self->{delta_k2}[$q][$p]) if $self->{debug};
       }
    }
+
    $self->{delta_b2} = [];
    foreach my $q (0 .. $self->{c2_kernels} - 1) {
       $self->{delta_b2}[$q][0] = 0;
       foreach my $i (0 .. $self->{c2_size} - 1) {
          foreach my $j (0 .. $self->{c2_size} - 1) {
-            say "self->{delta_b2}[$q][0] = " .  $self->{delta_b2}[$q][0] . " + " . $self->{delta_c2_sigmoid}[$q][$i][$j] if $self->{debug};
             $self->{delta_b2}[$q][0] += $self->{delta_c2_sigmoid}[$q][$i][$j];
+say "delta_b2 $q adding delta_c2_sigmoid $q $i $j " . $self->{delta_c2_sigmoid}[$q][$i][$j] . " = " . $self->{delta_b2}[$q][0] if $self->{debug};
          }
       }
    }
+
    foreach my $q (0 .. $self->{c2_kernels} - 1) {
       foreach my $p (0 .. $self->{c1_kernels} - 1) {
          print_2d_array("delta_c2_sigmoid $q", $self->{delta_c2_sigmoid}[$q]) if $self->{debug};
@@ -346,14 +367,6 @@ sub backward {
          }
       }
    }
-
-   foreach my $u (0 .. $self->{s1_size} - 1) {
-      foreach my $v (0 .. $self->{s1_size} - 1) {
-         foreach my $p (0 .. $self->{c1_kernels}) {
-             $self->{p1_delta}[$p][$u][$v] = 0;
-         }
-      }
-   }
    foreach my $p (0 .. $self->{c1_kernels} - 1) {
       foreach my $q (0 .. $self->{c2_kernels} - 1) {
          $self->{p1_delta}[$p] = add_2_arrays($self->{p1_delta}[$p], conv2d($self->{delta_c2_sigmoid}[$q], $k2_rot->[$q][$p], "expand"));
@@ -362,6 +375,7 @@ sub backward {
    foreach my $p (0 .. $self->{c1_kernels} - 1) {
       print_2d_array("p1_delta $p",$self->{p1_delta}[$p]) if $self->{debug};
    }
+   
    $self->{delta_c1} ||= [];
 
    foreach my $p (0 .. $self->{c1_kernels} - 1) {
@@ -401,7 +415,6 @@ sub backward {
          }
       }
    }
-
    foreach my $p (0 .. $self->{c1_kernels} - 1) {
       $self->{delta_k1}[$p] = conv2d($I_rot, $self->{delta_c1_sigmoid}[$p], "reduce");
       print_2d_array("delta_k1 $p", $self->{delta_k1}[$p]) if $self->{debug};
@@ -480,6 +493,5 @@ sub update_weights_and_biases {
       $self->{biases}[2][$b] -= $params{learning_rate} * $self->{delta_bias_fc}[$b][0];
    }    
    print_1d_array("biases fc after update", $self->{biases}[2]) if $self->{debug};
-
 }
 1;
